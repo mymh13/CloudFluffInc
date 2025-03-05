10899124d099ee1974ed347327aa633d5239a24d
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-03-05 13:46:04 +0100

    added client side validation with data annotations

--- Models/Subscriber.cs
+++ Models/Subscriber.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CloudFluffInc.Models;
 
 public class Subscriber
 {
-    public string Name { get; set; }
-    public string Email { get; set; }
+    [Required]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 20 characters")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = string.Empty;
 }
